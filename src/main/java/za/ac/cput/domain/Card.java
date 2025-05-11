557b21ce0c2db57b4c6aa7e9066efc80ed6956f3
222369345 <222369345@mycput.ac.za>
2025-05-11 21:10:37 +0200

    Changes made in Card.

--- src/main/java/za/ac/cput/domain/Card.java
+++ src/main/java/za/ac/cput/domain/Card.java
@@ -5,7 +5,7 @@ package za.ac.cput.domain;
      Author: RI Onwutali (222369345)
      Date: 11 May 2025 */
 
-public class Card {
+public class Card  {
 
     private String cardNumber;
     private String cvv;
