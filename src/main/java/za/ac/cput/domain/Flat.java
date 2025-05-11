e8b48407cf5c2d943e12829a0cfd5ac370187551
rasmenis <rasmenisiya722@gmail.com>
2025-05-11 20:02:57 +0200

    Commit changes.

--- src/main/java/za/ac/cput/domain/Flat.java
+++ src/main/java/za/ac/cput/domain/Flat.java
@@ -60,6 +60,7 @@ public class Flat extends Address {
             this.addressId = addressId;
             return this;
         }
+
         public Builder setStreetNumber(String streetNumber) {
             this.streetNumber = streetNumber;
             return this;
