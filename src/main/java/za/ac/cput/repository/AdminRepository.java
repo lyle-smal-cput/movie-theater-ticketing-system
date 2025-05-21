170fbfcfbf420bf6cd28aa3ac931fbdf0a193ec1
lyle-smal-cput <223236012@mycput.ac.za>
2025-05-21 17:13:46 +0200

    Customer & User Repository #30

--- src/main/java/za/ac/cput/repository/AdminRepository.java
+++ src/main/java/za/ac/cput/repository/AdminRepository.java
@@ -1,8 +1,15 @@
 package za.ac.cput.repository;
 
+/* AdminRepository.java
+     Admin Repository interface
+     Author: LJ Smal (223236012)
+     Date: 21 May 2025 */
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Admin;
+import za.ac.cput.domain.Customer;
+import za.ac.cput.domain.User;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,14 +17,12 @@ import java.util.List;
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, String> {
 
+    Admin findByUserId(String userId);
     List<Admin> findByUsername(String username);
     List<Admin> findByFirstName(String firstName);
     List<Admin> findByLastName(String lastName);
     List<Admin> findByEmail(String email);
-
-    //Not sure yet about this one
-    //List<Admin> findByAddressId(String addressId);
-
+    List<Admin> findByAddressId(String addressId);
     List<Admin> findAdminByCellphoneNumber(String cellphoneNumber);
     List<Admin> findByGender(String gender);
     List<Admin> findByDateOfBirth(LocalDate dateOfBirth);
