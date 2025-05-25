8cfa7669cb9c49574229c1cf731c1fbc6fd6568e
Sufyaan88 <198489287+SufyaanR@users.noreply.github.com>
2025-05-25 21:33:05 +0200

    Added Service and Controller

---
+++ src/main/java/za/ac/cput/service/ISeatService.java
@@ -0,0 +1,9 @@
+package za.ac.cput.service;
+
+import za.ac.cput.domain.Seat;
+
+import java.util.List;
+
+public interface ISeatService extends IService<Seat, String> {
+    List<Seat> getAll();
+}
