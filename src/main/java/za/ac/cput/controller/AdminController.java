de6eca55da9b4c07dda5164b04ceac130408cc8f
SufyaanR <221075127@mycput.ac.za>
2025-10-22 12:42:03 +0200

    Updates

--- src/main/java/za/ac/cput/controller/AdminController.java
+++ src/main/java/za/ac/cput/controller/AdminController.java
@@ -3,31 +3,48 @@ package za.ac.cput.controller;
 AdminController.java
  Admin Controller class
  Author: LJ Smal (223236012)
-     Date: 25 May 2025 */
+ Date: 25 May 2025
+*/
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import za.ac.cput.config.JwtUtil;
 import za.ac.cput.domain.Admin;
 import za.ac.cput.service.AdminService;
 
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
-    private AdminService service;
+
+    private final AdminService service;
+    private final JwtUtil jwtUtil;
 
     @Autowired
-    public AdminController(AdminService service) {
+    public AdminController(AdminService service, JwtUtil jwtUtil) {
         this.service = service;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/create")
-    public Admin create(@RequestBody Admin admin) {
-        return service.create(admin);
-    }
+    public Object create(@RequestBody Admin admin) {
+        // Create admin first
+        Admin createdAdmin = service.create(admin);
 
+        // Generate a JWT token for the created admin
+        String token = jwtUtil.generateToken(createdAdmin.getUsername(), "ADMIN");
+
+        // Return both user info and token
+        return Map.of(
+                "userId", createdAdmin.getUserId(),
+                "username", createdAdmin.getUsername(),
+                "role", "ADMIN",
+                "token", token
+        );
+    }
 
     @GetMapping("/read/{userId}")
     public Admin read(@PathVariable Long userId) {
@@ -35,7 +52,7 @@ public class AdminController {
     }
 
     @GetMapping("/readByEmail/{email}")
-    public List <Admin> readByEmail(@PathVariable String email) {
+    public List<Admin> readByEmail(@PathVariable String email) {
         return service.readByEmail(email);
     }
 
